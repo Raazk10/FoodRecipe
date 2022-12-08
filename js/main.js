93d844e11f8ff4689f71730c90844c517ae804bb
Raazk10 <rakioraag10@gmail.com>
2022-12-08 21:16:31 +0100

    added note

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //reference
 //https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22649527#questions/
 
-/* While creating this project I followed the tutorial from udemy which I have given the reference above. In this project mostly classes are used instead of functions to export and import in the module, so I followed the same pattern. To code this project I watched the video several times and followed along. In that project tutor have used the parcel to build the project but I haven't done that. The api used in this project is created by tutor himself. After finsihing the project , I again watched the tutorial several times to write the comment and JSDoc which helped me to better understand what the codes are doing and what their functions are. This is not the fully functional project just for educational purpose.*/
+/* While creating this project I followed the tutorial from udemy which I have given the reference above. In this project mostly classes are used instead of functions to export and import in the module, so I followed the same pattern. To code this project I watched the video several times and followed along. In that project tutor have used the parcel to build the project but I haven't done that. The api used in this project is created by tutor himself. After finsihing the project , I again watched the tutorial several times to write the comment and JSDoc which helped me to better understand what the codes are doing and what their functions are. This is not the fully functional project just for educational purpose. In the future I will try to follow all the rules and implement it in the upcoming project that I have learned from the class lecture.*/
 
 import Controller from "./controller.js";
 
